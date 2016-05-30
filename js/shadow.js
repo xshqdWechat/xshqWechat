c5343478977713f7337127fb57ae1c2318908673
huangjian <106025154@qq.com>
2016-05-30 17:18:50 +0800

    NEW

--- js/shadow.js
+++ js/shadow.js
@@ -7,9 +7,11 @@
      * @param {function} cloFun   关闭时回调函数
      * @param {function} okFun    确认时回调函数
      */
-    function shadow(popUpDiv, closeBtn,okBtn,calFun,okFun) {
+    function shadow(popDivPos,popUpDiv, closeBtn, okBtn, calFun, okFun) {
         var that = this;
         this.isOpen = false;
+        this.position = popDivPos || 'bottom';
+        console.log(this.position);
         if (popUpDiv !== undefined && typeof popUpDiv !== 'string') {
             Error.message = '需要传入弹出的层';
             throw Error;
@@ -18,22 +20,39 @@
             Error.message = '需要传入负责关闭层的的元素';
             throw Error;
         }
-        init(that, popUpDiv, closeBtn,okBtn,calFun,okFun);
+        init(that, popUpDiv, closeBtn, okBtn, calFun, okFun);
     }
 
-    function openWin(pc, sc, shadowDiv, pud) {
+    function openWin(that, pc, sc, shadowDiv, pud) {
         shadowDiv.appendTo('body');
+        sc.top = getH() - sc.height;
+        shadowDiv.css(sc);
+        //        默认底部
         pc.top = getH() - $(pud).height();
+        
+        if (that.position=='top') {
+            //        顶部
+            pc.top = getH() - getH(2)- $(pud).height();
+        } else if (that.position=='center') {
+            //        居中
+            pc.top = getH() - getH(2) / 2 - $(pud).height() / 2;
+        }
+
         pc.display = 'block';
         $(pud).css(pc);
         $('body').addClass('ovN');
-
+        //点击遮罩层，关闭
+        shadowDiv.on('click', function () {
+            closeWin(that, pc, sc, shadowDiv, pud);
+        });
+        that.isOpen = false;
     }
 
-    function closeWin(pc, sc, shadowDiv, pud) {
+    function closeWin(that, pc, sc, shadowDiv, pud) {
         shadowDiv.remove();
         pc.top = getH(1);
         $(pud).css(pc);
+        that.isOpen = false;
         setTimeout(function () {
             $(pud).css('display', 'none');
             $('body').removeClass('ovN');
@@ -49,11 +68,12 @@
         st = $(window).scrollTop();
         th = $(document).height();
 
-        if (getTH) return th;
+        if (getTH == 1) return th;
+        if (getTH == 2) return wh;
         return wh + st;
     }
 
-    function init(that, pud, cbtn,okBtn,calFun,okFun) {
+    function init(that, pud, cbtn, okBtn, calFun, okFun) {
         var pc = {},
             sc = {},
             ww, wh,
@@ -69,9 +89,10 @@
         sc.backgroundColor = '#000';
         sc.opacity = .7;
         sc.zIndex = 1200;
-        sc.top = 0;
+        sc.top = getH() - wh;
         shadowDiv.css(sc);
         //        计算需要弹出层放置最底部
+        pc.transition = 'top ease-in-out 0.26s';
         pc.top = getH(1);
         pc.position = 'absolute';
         pc.display = 'none';
@@ -80,17 +101,15 @@
 
         //        绑定关闭按钮
         $(cbtn).on('click', function () {
-            closeWin(pc, sc, shadowDiv, pud);
-            that.isOpen = false;
-            if(calFun){
+            closeWin(that, pc, sc, shadowDiv, pud);
+            if (calFun) {
                 calFun();
             }
         });
-        
-        $(okBtn).on('click',function(){
-            closeWin(pc, sc, shadowDiv, pud);
-            that.isOpen = false;
-            if(okFun){
+
+        $(okBtn).on('click', function () {
+            closeWin(that, pc, sc, shadowDiv, pud);
+            if (okFun) {
                 okFun();
             }
         })
@@ -98,15 +117,14 @@
         //        绑定弹出
         $(that).on('click', function () {
             if (that.isOpen) {
-                closeWin(pc, sc, shadowDiv, pud);
-                that.isOpen = false;
+                closeWin(that, pc, sc, shadowDiv, pud);
             } else {
-                openWin(pc, sc, shadowDiv, pud);
-                that.isOpen = true;
+                openWin(that, pc, sc, shadowDiv, pud);
             }
         })
+
     }
-    
+
     $.fn.shadow = shadow;
-    
+
 }(jQuery))
